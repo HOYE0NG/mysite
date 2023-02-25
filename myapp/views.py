fc9561abbe985b8d823b7dd970a71395d7ae1179
HOYE0NG <hy8839@naver.com>
2023-02-25 22:15:12 +0900

    commit

--- myapp/views.py
+++ myapp/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'myapp/index.html')
+    content = request.POST.get('content')
+    if content:
+        if '사랑해' in content:
+            content = "나도 사랑해 ㅎㅎ"
+    else:
+        content = "아무것도 안쓰네 ㅋ"
+    context = {
+        'content': content
+    }
+    return render(request, 'myapp/index.html', context)
