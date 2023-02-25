8711dea1ca3451d184378e43e5f978315544cfbf
HOYE0NG <hy8839@naver.com>
2023-02-25 22:29:17 +0900

    index.html modfied

--- myapp/views.py
+++ myapp/views.py
@@ -3,12 +3,19 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    content = request.POST.get('content')
-    if content:
-        if '사랑해' in content:
-            content = "나도 사랑해 ㅎㅎ"
-    else:
-        content = "아무것도 안쓰네 ㅋ"
+    content = ""
+    if request.method == "POST":
+        content = request.POST.get('content')
+        if content:
+            if '사랑해' in content:
+                content = "나도 사랑해 ㅎㅎ"
+            if '보고싶' in content:
+                content = "그럼 빨리 와야지ㅋ"
+            else:
+                content = "듣고 싶은 말이 아님ㅋ"
+        else:
+            content = "아무것도 안쓰네 ㅋ"
+
     context = {
         'content': content
     }
